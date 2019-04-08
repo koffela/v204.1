3123e71fc1bd7c8c10ad24ae88c7bfddb6168625
Tim <tim.blokk@gmail.com>
2019-04-08 14:23:15 +0200

    Fixed equip encode + cash item crashes.
    Fixed cash shop MoveStoL.
    
    (cherry picked from commit 4e6829c9004f81e8a29ddd83964067822e647021)

--- src/main/java/net/swordie/ms/client/character/items/Equip.java
+++ src/main/java/net/swordie/ms/client/character/items/Equip.java
@@ -1172,20 +1172,19 @@ public class Equip extends Item {
             // sockets 0 through 2 (-1 = none, 0 = empty, >0 = filled
             outPacket.encodeShort(getSocket(i));
         }
-        outPacket.encodeInt(0); // ?
-        // if (!v3->liCashItemSN.QuadPart) {
+        if (!isCash()) {
             outPacket.encodeLong(getId());
-        // }
-        outPacket.encodeFT(FileTime.fromType(FileTime.Type.MAX_TIME));
-        outPacket.encodeInt(-1);//nPrevBonusExpRate
-        // GW_CashItemOption
-        outPacket.encodeLong(getId());
-        outPacket.encodeFT(getDateExpire());
-        outPacket.encodeInt(0);// grade
-        for (int i = 0; i < 3; i++) {
-            outPacket.encodeInt(0);
         }
-        // end GW_CashItemOption
+        outPacket.encodeFT(FileTime.fromType(FileTime.Type.MAX_TIME)); // ftEquipped
+        outPacket.encodeInt(-1); // nPrevBonusExpRate
+        // GW_CashItemOption::Decode
+        outPacket.encodeLong(isCash() ? 0 : getId()); // cash sn already encoded in the super's encode
+        outPacket.encodeFT(FileTime.fromType(FileTime.Type.MAX_TIME)); // ftDateExpire
+        outPacket.encodeInt(getGrade());
+        for (int i = 0; i < 3; i++) {
+            outPacket.encodeInt(getOptionBase(i));
+        }
+        // ~
         outPacket.encodeShort(getSoulOptionId()); // soul ID
         outPacket.encodeShort(getSoulSocketId()); // enchanter ID
         outPacket.encodeShort(getSoulOption()); // optionID (same as potentials)

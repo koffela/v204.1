b68fde923f44447145c185cc29ea3883e409502b
g1zstar <sensiblemagic@gmail.com>
2019-08-04 02:57:29 -0400

    Shade 1st job skill Flash Fist is now learned at whatever level swift strike was before replacing it. Fixed Griffey and Manon 4th job advance portals Fixed Outlaw job advancement
    Evan tutorial no longer receives two lunch boxes instead of one
    
    (cherry picked from commit dddddf575a43714675404b324e9b182282147689)

--- scripts/quest/q22003s.py
+++ scripts/quest/q22003s.py
@@ -2,7 +2,6 @@
 sm.setSpeakerID(1013100)
 if sm.sendAskAccept("Your #bDad#k forgot his Lunch Box when he left for the farm this morning. Will you #bdeliver this Lunch Box#k to your Dad in #b#m100030300##k, honey?"):
     if sm.canHold(4032448):
-        sm.giveItem(4032448, 1)
         sm.startQuest(parentID)
 
         sm.sendNext("Heehee, my Evan is such a good kid! Head #bleft after you exit the house#k. Rush over to your dad. I'm sure he's starving.")

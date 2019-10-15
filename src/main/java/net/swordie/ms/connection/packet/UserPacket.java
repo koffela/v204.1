0721e7d4c0542c68fbfec482dc8455b3138c25aa
jordan <ruejrugh2@gmail.com>
2019-10-15 11:12:44 +0300

    -forgot to add this last commit
    (cherry picked from commit f2c5efc3c62aa9aa952a0192bfb96b78e176a826)

--- src/main/java/net/swordie/ms/connection/packet/UserPacket.java
+++ src/main/java/net/swordie/ms/connection/packet/UserPacket.java
@@ -197,11 +197,10 @@ public class UserPacket {
         return outPacket;
     }
 
-    public static OutPacket checkUpgradeItemResult(int index) {
+    public static OutPacket checkUpgradeItemResult(int index, boolean show) {
         OutPacket outPacket = new OutPacket(OutHeader.EGO_EQUIP_CHECK_UPGRADE_ITEM_RESULT);
-
-        outPacket.encodeByte(true);
-        outPacket.encodeString("");
+        outPacket.encodeByte(show);
+        outPacket.encodeString(""); //does nothing and not named in kmst idb.
         outPacket.encodeInt(index);
         return outPacket;
     }

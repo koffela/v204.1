0e14c0a6457a3472dd8a9b8b9c235256f1354a9a
Descended <41481611+Descended@users.noreply.github.com>
2020-08-20 10:52:12 +0200

    Updated public void heal
    (cherry picked from commit a66f35d3de7eb3d9defdecb65bafdc47be05f6c5)

--- src/main/java/net/swordie/ms/client/character/Char.java
+++ src/main/java/net/swordie/ms/client/character/Char.java
@@ -3248,10 +3248,7 @@ public class Char {
 		int newHP = curHP + amount > maxHP ? maxHP : curHP + amount;
 		Map<Stat, Object> stats = new HashMap<>();
 
-		if (whilstDeath) {
-			setStat(Stat.hp, newHP);
-			stats.put(Stat.hp, newHP);
-		} else if(getHP() > 0) {
+		if(whilstDeath || getHP() > 0) {
 			setStat(Stat.hp, newHP);
 			stats.put(Stat.hp, newHP);
 		}

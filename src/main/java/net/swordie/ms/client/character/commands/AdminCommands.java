ryantpayton/Swordie

- handling mobs reaching the middle of the map in gollux's head stage - portals now open for abdomen and shoulders - added a function to respawn mobs repeatitly and spawning gollux dummies for head stage
todo:
- make the right mob at the head stage go to the middle of the map
- find right cts for gollux stack count
- block gollux hand attacks at head stage according to shoulders cleared.

src/main/java/net/swordie/ms/client/character/commands/AdminCommands.java
@@ -48,6 +48,7 @@ import java.time.LocalDateTime;
 import java.util.*;
 
 import static net.swordie.ms.client.character.skills.temp.CharacterTemporaryStat.RideVehicle;
+import static net.swordie.ms.client.character.skills.temp.CharacterTemporaryStat.Unk459;
 import static net.swordie.ms.enums.AccountType.*;
 import static net.swordie.ms.enums.ChatType.*;
 import static net.swordie.ms.enums.InventoryOperation.Add;
@@ -62,8 +63,20 @@ public class AdminCommands {
     public static class Test extends AdminCommand {
 
         public static void execute(Char chr, String[] args) {
-            Android andr = new Android(Integer.parseInt(args[1]), chr, (short) 0, (short) 1000, (short) 1000, "Sword");
-            chr.write(AndroidPacket.created(andr));
+            Option o = new Option();
+            o.nOption = 1;
+            o.rOption = 800;
+            o.tOption = 5; // so it fades away
+
+            CharacterTemporaryStat cts = CharacterTemporaryStat.getByBitPos(Integer.parseInt(args[1]));
+            if (cts == null) {
+                chr.chatMessage("Could not find cts with bitpos " + args[1]);
+                return;
+            }
+            TemporaryStatManager tsm = chr.getTemporaryStatManager();
+            tsm.putCharacterStatValue(cts, o);
+            tsm.sendSetStatPacket();
+            System.out.println(String.format("CTS %s = %s", args[1], cts));
         }
     }
 

koffela/v204.1

(cherry picked from commit 6e1387c3c50746a3d40f015a1d33e23119fd3599)

src/main/java/net/swordie/ms/client/character/commands/AdminCommands.java
@@ -61,6 +61,8 @@ import static net.swordie.ms.client.character.skills.SkillStat.prop;
 import static net.swordie.ms.client.character.skills.temp.CharacterTemporaryStat.*;
 import static net.swordie.ms.client.character.skills.temp.CharacterTemporaryStat.Stance;
 import static net.swordie.ms.enums.PrivateStatusIDFlag.*;
+
+import static net.swordie.ms.client.character.skills.temp.CharacterTemporaryStat.RideVehicle;
 import static net.swordie.ms.enums.ChatType.*;
 import static net.swordie.ms.enums.InventoryOperation.Add;
 
@@ -89,18 +91,20 @@ public class AdminCommands {
     public static class TestPacket extends AdminCommand {
 
         public static void execute(Char chr, String[] args) {
-            if (args.length < 3) {
-                chr.chatMessage("Usage: !packet <op> <data>");
+            Option o = new Option();
+            o.nOption = 1;
+            o.rOption = 800;
+            o.tOption = 5; // so it fades away
+
+            CharacterTemporaryStat cts = CharacterTemporaryStat.getByBitPos(Integer.parseInt(args[1]));
+            if (cts == null) {
+                chr.chatMessage("Could not find cts with bitpos " + args[1]);
                 return;
             }
-            OutPacket outPacket = new OutPacket(Short.parseShort(args[1]));
-            StringBuilder data = new StringBuilder();
-            for (int i = 2; i < args.length; i++) {
-                data.append(" ").append(args[i]);
-            }
-            outPacket.encodeArr(data.toString());
-            chr.write(outPacket);
-
+            TemporaryStatManager tsm = chr.getTemporaryStatManager();
+            tsm.putCharacterStatValue(cts, o);
+            tsm.sendSetStatPacket();
+            System.out.println(String.format("CTS %s = %s", args[1], cts));
         }
     }
 
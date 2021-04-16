ryantpayton/Swordie

Made changes according to comments. Added some generic map effect scripts. Fixed returnmap storage when traveling between FM and Ardentmill.

src/main/java/net/swordie/ms/handlers/social/ChatHandler.java
@@ -41,17 +41,18 @@ public class ChatHandler {
         inPacket.decodeInt(); // timestamp
         String msg = inPacket.decodeString();
 
-        if (msg.length() <= 0)
+        if (msg.length() <= 0) {
             return;
+        }
 
         String command = msg.split(" ")[0].replace(String.valueOf(msg.charAt(0)), "");
         Class[] commandClasses = null;
 
-        if (msg.startsWith(String.valueOf(ServerConfig.PLAYER_COMMAND)))
+        if (msg.startsWith(String.valueOf(ServerConfig.PLAYER_COMMAND))) {
             commandClasses = PlayerCommands.class.getClasses();
-
-        if (msg.startsWith(String.valueOf(ServerConfig.ADMIN_COMMAND)))
+        } else if (msg.startsWith(String.valueOf(ServerConfig.ADMIN_COMMAND))) {
             commandClasses = AdminCommands.class.getClasses();
+        }
 
         // doesn't start with command prefix
         if (commandClasses == null) {
@@ -64,11 +65,13 @@ public class ChatHandler {
             Command cmd = (Command) commandClass.getAnnotation(Command.class);
             for (String name : cmd.names()) {
 
-                if (!name.equalsIgnoreCase(command))
+                if (!name.equalsIgnoreCase(command)) {
                     continue;
+                }
 
-                if (chr.getUser().getAccountType().ordinal() < cmd.requiredType().ordinal())
-                    break;
+                if (chr.getUser().getAccountType().ordinal() < cmd.requiredType().ordinal()) {
+                    continue;
+                }
 
                 try {
                     ICommand iCommand = null;

koffela/v204.1

(cherry picked from commit 4084db6f45c2b152aca4b5d5899f77feaee0b6f5)

src/main/java/net/swordie/ms/handlers/social/ChatHandler.java
@@ -30,17 +30,18 @@ public class ChatHandler {
         inPacket.decodeInt(); // timestamp
         String msg = inPacket.decodeString();
 
-        if (msg.length() <= 0)
+        if (msg.length() <= 0) {
             return;
+        }
 
         String command = msg.split(" ")[0].replace(String.valueOf(msg.charAt(0)), "");
         Class[] commandClasses = null;
 
-        if (msg.startsWith(String.valueOf(ServerConfig.PLAYER_COMMAND)))
+        if (msg.startsWith(String.valueOf(ServerConfig.PLAYER_COMMAND))) {
             commandClasses = PlayerCommands.class.getClasses();
-
-        if (msg.startsWith(String.valueOf(ServerConfig.ADMIN_COMMAND)))
+        } else if (msg.startsWith(String.valueOf(ServerConfig.ADMIN_COMMAND))) {
             commandClasses = AdminCommands.class.getClasses();
+        }
 
         // doesn't start with command prefix
         if (commandClasses == null) {
@@ -53,11 +54,13 @@ public class ChatHandler {
             Command cmd = (Command) commandClass.getAnnotation(Command.class);
             for (String name : cmd.names()) {
 
-                if (!name.equalsIgnoreCase(command))
+                if (!name.equalsIgnoreCase(command)) {
                     continue;
+                }
 
-                if (chr.getUser().getPrivateStatusIDFlag().ordinal() < cmd.requiredType().ordinal())
-                    break;
+                if (chr.getUser().getPrivateStatusIDFlag().ordinal() < cmd.requiredType().ordinal()) {
+                    continue;
+                }
 
                 try {
                     ICommand iCommand = null;
not-ebx/mugemu

change maplepoint to nx

src/main/java/net/swordie/ms/handlers/user/MigrationHandler.java
@@ -114,7 +114,7 @@ public class MigrationHandler {
         chr.initBaseStats();
         chr.setOnline(true); // v195+: respect 'invisible login' setting
         chr.getOffenseManager().setChr(chr);
-        c.write(WvsContext.setMaplePoint(user.getMaplePoints()));
+        c.write(WvsContext.setMaplePoint(acc.getNxCredit()));
     }
 
 

koffela/v204.1

(cherry picked from commit eb7983ddb04df4f97e8fba89b064978cf0d3576d)

src/main/java/net/swordie/ms/handlers/user/MigrationHandler.java
@@ -152,7 +152,7 @@ public class MigrationHandler {
         chr.setOnline(true); // v195+: respect 'invisible login' setting
         chr.getOffenseManager().setChr(chr);
         MatrixInventory.reloadSkills(chr);
-        c.write(WvsContext.setMaplePoints(user.getMaplePoints()));
+        c.write(WvsContext.setMaplePoints(acc.getNxCredit()));
     }
 
 
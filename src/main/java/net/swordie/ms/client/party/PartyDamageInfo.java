not-ebx/mugemu

send MaplePoint update to client

src/main/java/net/swordie/ms/client/party/PartyDamageInfo.java
@@ -61,7 +61,8 @@ public class PartyDamageInfo {
             }
             if (Util.succeedProp(GameConstants.NX_DROP_CHANCE)) {
                 int nx = (int) (perc * mob.getNxDropAmount());
-                chr.addNx(nx);
+//                chr.addNx(nx);
+                chr.addMaplePoint(nx);
             }
         }
     }

koffela/v204.1

(cherry picked from commit ee7017107b4331fd87b3f97509f5bfdc87bbcefa)

src/main/java/net/swordie/ms/client/party/PartyDamageInfo.java
@@ -61,7 +61,8 @@ public class PartyDamageInfo {
             }
             if (Util.succeedProp(GameConstants.NX_DROP_CHANCE)) {
                 int nx = (int) (perc * mob.getNxDropAmount());
-                chr.addNx(nx);
+//                chr.addNx(nx);
+                chr.addMaplePoints(nx);
             }
         }
     }
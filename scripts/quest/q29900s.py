7550fc9deb2bb5c116269d836ab02e2715f1dc63
Pyonta <furi.curi36@gmail.com>
2021-05-21 23:02:04 -0400

    * (Mostly) implemented Phantom's job advancements A bunch of Phantom's 2nd job scripts were refactored, while his 3rd and 4th are now mostly implemented, save for the Dust Dwarfs spawns during his 3rd job quest. Also fixed Phantom losing access to his own vaults after hitting 4th job. * Refactored and fixed a few scripts in Phantom's tutorial. Phantom can now skip his tutorial after the initial monologue (this can re-fire by logging back in). * Editted all existing, remaining auto complete job medal quests to specify the medal's actual name * Reformatted isAdventurerArcher() so it actually aligns with the other Explorer JobConstants bool functions
    (cherry picked from commit 260859be1aa66c95e16721fc21811ee488e72243)

--- scripts/quest/q29900s.py
+++ scripts/quest/q29900s.py
@@ -1,11 +1,8 @@
 # Beginner Adventurer
-# Created by MechAviv
-# Quest ID :: 29900
-# Not coded yet
 
 medal = 1142107
 
 if sm.canHold(medal):
-    sm.systemMessage("You have earned the <Beginner Adventurer> title.")
+    sm.chatScript("You obtained the <Beginner Adventurer> medal.")
     sm.startQuest(parentID)
-    sm.completeQuestNoRewards(parentID, True)
+    sm.completeQuest(parentID)
